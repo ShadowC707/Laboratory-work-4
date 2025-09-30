92120ff4004eb93e7a0d8d05208ff5a59e2015e3
Dobrynin Oleksii <dobrynin.oleksii707@gmail.com>
2025-09-30 17:27:00 +0300

    Added console menu and exceptions

--- src/NegativeCaloriesException.java
+++
@@ -1,5 +0,0 @@
-public class NegativeCaloriesException extends Exception {
-    public NegativeCaloriesException(String message) {
-        super(message);
-    }
-}
--- src/Salad.java
+++ src/Salad.java
@@ -11,6 +11,19 @@ public class Salad {
         this.ingredients = new ArrayList<>();
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Vegetable> getIngredients() {
+        return ingredients;
+    }
+    public void setIngredients(List<Vegetable> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     // methods
     public int getTotalCalories() throws EmptySaladException {
         if(this.ingredients.isEmpty()){
